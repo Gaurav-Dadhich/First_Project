87872ca6555d70de4755db37add55b5c52117c77
Gaurav-Dadhich <kryptogaurav@gmail.com>
2024-06-12 11:30:24 +0530

    Forth Commit.

--- app/src/main/java/com/example/first_project/MainActivity.kt
+++ app/src/main/java/com/example/first_project/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.first_project
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import androidx.activity.enableEdgeToEdge
@@ -23,14 +24,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    val Re = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        Log.i(Re,"Activity Starts!!!")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i(Re,"Activity Stops!")
+    }
+    //Similarly we have pause, destroy and resume functions for the activity cycle.
+
     fun clickHandler(view: View) {
         Log.i("MainActivity-clickHandler","Button Clicked")
 //        var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:9782915123"))
 //        var webIntent: Intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"))
 //        startActivity(webIntent)
 
-       /* var hintent = Intent(this.HomeActivity::class.java)
-        hintent.putExtra(name:"nkey", value:"abdul-android")
+        /*var hintent = Intent(this,HomeActivity::class.java)
+        hintent.putExtra(name:"nykey", value:"abdul-android")
         startActivity(hintent)*/
     }
 
